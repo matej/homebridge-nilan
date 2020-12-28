f47ec2baeb094a30bf042de2b5da93ce442bfa3d
Matej Bukovinski <matej@bukovinski.com>
2020-12-28 20:32:07 +0100

    Rename platform

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'NilanCompactP';
+export const PLATFORM_NAME = 'Nilan';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-nilan-compact-p';
+export const PLUGIN_NAME = 'homebridge-nilan';
